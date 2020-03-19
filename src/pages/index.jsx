4a3b01d16c5d827734238f009ff731920f5fcb7b
Duncan Bain <duncan@bain.io>
2020-03-19 17:06:53 +0000

    styling and removal of post exercpt

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,17 +8,16 @@ const IndexTemplate = ({ data }) => (
     <Layout>
       <section className="px-8 py-4 md:py-8">
         {data.allMdx.nodes.map(({
-          id, excerpt, frontmatter, fields,
+          id, frontmatter, fields,
         }) => (
           <article key={id} className="mb-24">
             <Link to={fields.slug}>
               <h1 className="mb-2">
-                <span className="font-title text-2r text-gray-800 leading-snug">
+                <span className="font-title text-4xl text-gray-800 leading-snug">
                   {frontmatter.title}
                   {' '}
                 </span>
                 <p>{frontmatter.date}</p>
-                <p>{excerpt}</p>
               </h1>
             </Link>
           </article>
