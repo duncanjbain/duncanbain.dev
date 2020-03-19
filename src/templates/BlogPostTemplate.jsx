f4fdf36c734c2eb88422c33fd6e7367e756ca629
Duncan Bain <duncan@bain.io>
2020-03-19 20:20:19 +0000

    markdown formatting, post description and styling, markdown example added

--- src/templates/BlogPostTemplate.jsx
+++ src/templates/BlogPostTemplate.jsx
@@ -10,12 +10,15 @@ export default ({ data, pageContext }) => {
   const { previous, next } = pageContext;
   return (
     <Layout>
-      <article className="markdown">
-        <h1>{frontmatter.title}</h1>
-        <p>{frontmatter.date}</p>
-        <MDXRenderer>
-          {body}
-        </MDXRenderer>
+      <article className="m-8 break-words md:mt-12 mb-16">
+        <h2 className="text-3xl text-gray-800 font-medium leading-snug mb-2">{frontmatter.title}</h2>
+        <p className="text-lg text-gray-800 font-light mb-2">{frontmatter.description}</p>
+        <p className="text-xs text-gray-500 font-thin">{frontmatter.date}</p>
+        <div className="markdown">
+          <MDXRenderer>
+            {body}
+          </MDXRenderer>
+        </div>
         {previous === false ? null : (
           <>
             {previous && (
@@ -45,6 +48,7 @@ export const query = graphql`
       body
       frontmatter {
         title
+        description
         date(formatString: "YYYY MMMM Do")
       }
     }
