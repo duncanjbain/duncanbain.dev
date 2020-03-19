6b56d69ea44bdcfa9d9caf79cb861f6121414e9d
Duncan Bain <duncan@bain.io>
2020-03-19 12:31:20 +0000

    blog post content now showing

--- src/templates/blogPostTemplate.jsx
+++ src/templates/blogPostTemplate.jsx
@@ -1,9 +1,27 @@
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx';
 import React from 'react';
+import Layout from '../components/Layout';
 
-const blogPostTemplate = () => (
-  <>
-    <p>Post here</p>
-  </>
-);
+export default ({ data }) => {
+  const { frontmatter, body } = data.mdx;
+  return (
+    <Layout>
+      <h1>{frontmatter.title}</h1>
+      <p>{frontmatter.date}</p>
+      <MDXRenderer>{body}</MDXRenderer>
+    </Layout>
+  );
+};
 
-export default blogPostTemplate;
+export const query = graphql`
+  query PostsBySlug($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      body
+      frontmatter {
+        title
+        date(formatString: "YYYY MMMM Do")
+      }
+    }
+  }
+`;
