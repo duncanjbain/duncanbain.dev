06119b18f03f0db0143ba5954dcea37f04ac3150
Duncan Bain <duncan@bain.io>
2020-03-27 09:37:14 +0000

    styled navar items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,33 +18,54 @@ const Header = ({
     </Link>
     <nav>
       <ul className="flex mt-4 md:md-0">
-        <li className="mr-4">
-          <Link to="/about" className="text-blue-700 hover:text-blue-500">
+        <li className="mr-2">
+          <Link
+            to="/about"
+            className="text-blue-700 hover:text-blue-500 border-b-2 border-transparent p-2 hover:border-blue-400"
+          >
             About
           </Link>
         </li>
-        <li className="mr-4">
-          <Link to="/projects" className="text-blue-700 hover:text-blue-500">
+        <li className="mr-2">
+          <Link
+            to="/projects"
+            className="text-blue-700 hover:text-blue-500 border-b-2 border-transparent p-2 hover:border-blue-400"
+          >
             Projects
           </Link>
         </li>
-        <li className="mr-4">
-          <Link to="/uses" className="text-blue-700 hover:text-blue-500">
+        <li className="mr-2">
+          <Link
+            to="/uses"
+            className="text-blue-700 hover:text-blue-500 border-b-2 border-transparent p-2 hover:border-blue-400"
+          >
             Uses
           </Link>
         </li>
-        <li className="mr-4">
-          <a href={twitterLink} aria-label="Twitter Profile">
+        <li className="mr-2">
+          <a
+            href={twitterLink}
+            aria-label="Twitter Profile"
+            className="text-blue-700 hover:text-blue-500 border-b-2 border-transparent p-2 hover:border-blue-400"
+          >
             <FontAwesomeIcon icon={['fab', 'twitter']} />
           </a>
         </li>
-        <li className="mr-4">
-          <a href={githubLink} aria-label="Github Profile">
+        <li className="mr-2">
+          <a
+            href={githubLink}
+            aria-label="Github Profile"
+            className="text-blue-700 hover:text-blue-500 border-b-2 border-transparent p-2 hover:border-blue-400"
+          >
             <FontAwesomeIcon icon={['fab', 'github']} />
           </a>
         </li>
-        <li className="mr-4">
-          <a href={linkedinLink} aria-label="LinkedIn Profile">
+        <li className="mr-2">
+          <a
+            href={linkedinLink}
+            aria-label="LinkedIn Profile"
+            className="text-blue-700 hover:text-blue-500"
+          >
             <FontAwesomeIcon icon={['fab', 'linkedin']} />
           </a>
         </li>
