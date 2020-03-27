4f3d6fa531d62b758a99823055c2000e918aacf6
Duncan Bain <duncan@bain.io>
2020-03-27 08:39:03 +0000

    CSS tidy up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = ({
   githubLink,
   linkedinLink,
 }) => (
-  <header className="px-8 pt-10 flex flex-col md:flex-row md:items-end md:justify-start md:content-end">
+  <header className="mx-8 mt-10 flex flex-col md:flex-row md:items-end md:justify-start md:content-end">
     <Link to="/">
       <h1 className="text-xl text-gray-800 font-medium flex flex-col">
         {siteAuthor}
@@ -19,24 +19,24 @@ const Header = ({
     <nav>
       <ul className="flex mt-4 md:md-0">
         <li>
-          <span className="px-4 text-gray-500 hidden md:block">|</span>
+          <span className="mx-4 text-gray-500 hidden md:block">|</span>
         </li>
         <li className="mr-4">
           <Link to="/about" className="text-blue-700 hover:text-blue-500">
             About
           </Link>
         </li>
-        <li className="align-middle mr-4">
+        <li className="mr-4">
           <a href={twitterLink} aria-label="Twitter Profile">
             <FontAwesomeIcon icon={['fab', 'twitter']} />
           </a>
         </li>
-        <li className="align-middle mr-4">
+        <li className="mr-4">
           <a href={githubLink} aria-label="Github Profile">
             <FontAwesomeIcon icon={['fab', 'github']} />
           </a>
         </li>
-        <li className="align-middle mr-4">
+        <li className="mr-4">
           <a href={linkedinLink} aria-label="LinkedIn Profile">
             <FontAwesomeIcon icon={['fab', 'linkedin']} />
           </a>
