90a17dce990ab5f86b1a73afa11e7e6297c5a82c
Duncan Bain <duncan@bain.io>
2020-04-07 00:07:15 +0100

    move styling mdx content from CSS file to styled-components passed to MDXProvider

--- src/templates/BlogPostTemplate.jsx
+++ src/templates/BlogPostTemplate.jsx
@@ -1,5 +1,6 @@
 import { graphql, Link } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { MDXProvider } from "@mdx-js/react";
 import React from "react";
 import styled from "styled-components";
 import tw from "tailwind.macro";
@@ -21,6 +22,57 @@ const ArticleDescription = styled.p`
 const ArticleDate = styled.p`
   ${tw`text-xs text-gray-500 font-thin`}
 `;
+const ArticleH2 = styled.h2`
+  ${tw`text-gray-800 mt-6 mb-2 -ml-1 text-xl font-extrabold tracking-wider`}
+`;
+
+const ArticleH3 = styled.h3`
+  ${tw`text-gray-800 mt-6 mb-2 -ml-1 text-lg font-bold`}
+`;
+
+const ArticleH4 = styled.h4`
+  ${tw`text-gray-800 mt-6 mb-2 -ml-1 text-lg font-semibold`}
+`;
+
+const ArticleH5 = styled.h5`
+  ${tw`text-gray-800 mt-6 mb-2 -ml-1 text-lg font-medium`}
+`;
+
+const ArticleH6 = styled.h6`
+  ${tw`text-gray-800 mt-6 mb-2 -ml-1 text-base font-semibold`}
+`;
+
+const ArticlePara = styled.p`
+  ${tw`text-gray-800 leading-relaxed font-light mt-2 mb-4`}
+`;
+
+const ArticleBlockQuote = styled.blockquote`
+  ${tw`border-solid border-l-4 border-gray-300 pl-4`}
+`;
+
+const ArticleUl = styled.ul`
+  ${tw`list-disc m-4`}
+`;
+
+const ArticleOl = styled.ol`
+  ${tw`list-decimal m-4`}
+`;
+
+const ArticleLi = styled.li`
+  ${tw`text-gray-800 font-light my-1`}
+`;
+
+const ArticleStrong = styled.strong`
+  ${tw`font-semibold`}
+`;
+
+const ArticleEm = styled.em`
+  ${tw`italic`}
+`;
+
+const ArticleA = styled.a`
+  ${tw`text-blue-700 leading-relaxed font-medium mb-8 hover:text-blue-500`}
+`;
 
 export default ({ data, pageContext }) => {
   const { frontmatter, body } = data.mdx;
@@ -33,7 +85,28 @@ export default ({ data, pageContext }) => {
         <ArticleHeader>{frontmatter.title}</ArticleHeader>
         <ArticleDescription>{frontmatter.description}</ArticleDescription>
         <ArticleDate>{frontmatter.date}</ArticleDate>
-        <MDXRenderer>{body}</MDXRenderer>
+        <div className="markdown">
+          <MDXProvider
+            components={{
+              h1: ArticleH2, // H1 reserved for article semantic title
+              h2: ArticleH2,
+              h3: ArticleH3,
+              h4: ArticleH4,
+              h5: ArticleH5,
+              h6: ArticleH6,
+              p: ArticlePara,
+              blockquote: ArticleBlockQuote,
+              ul: ArticleUl,
+              ol: ArticleOl,
+              li: ArticleLi,
+              strong: ArticleStrong,
+              em: ArticleEm,
+              a: ArticleA,
+            }}
+          >
+            <MDXRenderer>{body}</MDXRenderer>
+          </MDXProvider>
+        </div>
         {previous === false ? null : (
           <>
             {previous && (
