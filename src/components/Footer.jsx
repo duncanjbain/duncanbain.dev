81a8d5d9574245b08af0c0d71c4e79f9e4f1ee67
Duncan Bain <duncan@bain.io>
2020-04-06 22:38:23 +0100

    convert Footer component styles to styled-components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,34 @@
 import { Link } from "gatsby";
+import styled from "styled-components";
+import tw from "tailwind.macro";
 import React from "react";
 
+const FooterContainer = styled.footer`
+  ${tw`m-4 mx-8 text-center border-t pt-4`}
+`;
+
+const FooterText = styled.p`
+  ${tw`text-base text-gray-700 font-light`}
+`;
+
+const FooterLink = styled(Link)`
+  ${tw`text-blue-700 hover:text-blue-500`}
+`;
+
 const Footer = () => (
-  <footer className="m-4 mx-8 text-center border-t pt-4">
-    <p className="text-base text-gray-700 font-light">
+  <FooterContainer>
+    <FooterText>
       Made with{" "}
       <span role="img" aria-label="Sparkly heart emoji">
         💖
       </span>{" "}
       by{" "}
-      <Link to="/" className="text-blue-700 hover:text-blue-500">
+      <FooterLink to="/" className="">
         Duncan Bain
-      </Link>{" "}
-      with{" "}
-      <a
-        href="https://gatsbyjs.org"
-        className="text-blue-700 hover:text-blue-500"
-      >
-        Gatsby
-      </a>
-    </p>
-  </footer>
+      </FooterLink>{" "}
+      with <FooterLink href="https://gatsbyjs.org">Gatsby</FooterLink>
+    </FooterText>
+  </FooterContainer>
 );
 
 export default Footer;
