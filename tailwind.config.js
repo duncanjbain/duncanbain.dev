7317d7fbd5b53021ef1f2d99ae4aa11e2fb83a67
Duncan Bain <duncan@bain.io>
2020-04-28 22:28:42 +0000

    updated font list to include system stack fallbacks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   theme: {
     fontFamily: {
-      sans: ["Inter"],
+      sans: [
+        "Inter",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica",
+        "Arial",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+      ],
     },
   },
   variants: {},
