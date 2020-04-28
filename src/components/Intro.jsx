c8f6dc207eb00361b8591b5f3d16b30d68ffdce4
Duncan Bain <duncan@bain.io>
2020-04-28 21:25:12 +0000

    updated intro/bio text and style

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,13 +1,22 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import tw from "tailwind.macro";
 import styled from "styled-components";
 
 const IntroContainer = styled.section`
-  ${tw`px-8 md:py-2`}
+  ${tw`px-8 md:py-2 leading-relaxed`}
 `;
 
 const IntroText = styled.p`
-  ${tw`text-4xl`}
+  ${tw`text-2xl md:text-4xl font-bold text-blue-600`}
+`;
+
+const Name = styled.span`
+  ${tw`bg-gray-200 px-2`}
+`;
+
+const BioText = styled.p`
+  ${tw`font-normal text-xl md:text-2xl text-current`}
 `;
 
 const Intro = () => (
@@ -17,7 +26,12 @@ const Intro = () => (
       <span role="img" aria-label="Waving hand emoji icon">
         👋
       </span>{" "}
-      I&apos;m Duncan
+      <Name>I'm Duncan</Name>
+      <BioText>
+        I'm a Mechanical Engineer who is learning to code. This where I share
+        things I've created and write about things I'm working on or anything
+        else that I find exciting.
+      </BioText>
     </IntroText>
   </IntroContainer>
 );
