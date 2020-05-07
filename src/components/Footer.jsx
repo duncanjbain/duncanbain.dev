16c8beb6d43eb5cbde6abd841148478d20fe4d18
Duncan Bain <duncan@bain.io>
2020-05-07 15:19:42 +0000

    footer responsive tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,32 +8,24 @@ const FooterContainer = styled.footer`
 `;
 
 const FooterText = styled.p`
-  ${tw`text-base text-gray-700 font-light`}
+  ${tw`text-base text-gray-700 font-light flex flex-row justify-center items-center`}
 `;
 
 const FooterLink = styled(Link)`
-  ${tw`text-blue-700 hover:text-blue-500`}
-`;
-
-const FooterExternalLink = styled.a`
-  ${tw`text-blue-700 hover:text-blue-500`}
+  ${tw`text-blue-700 hover:text-blue-500 mx-1`}
 `;
 
 const Footer = () => (
   <FooterContainer>
     <FooterText>
       Made with{" "}
-      <span role="img" aria-label="Sparkly heart emoji">
+      <span className="mx-1" role="img" aria-label="Sparkly heart emoji">
         💖
       </span>{" "}
       by{" "}
       <FooterLink to="/" className="">
         Duncan Bain
       </FooterLink>{" "}
-      with{" "}
-      <FooterExternalLink href="https://gatsbyjs.org">
-        Gatsby
-      </FooterExternalLink>
     </FooterText>
   </FooterContainer>
 );
