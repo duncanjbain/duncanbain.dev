cc6a789d7a39a2f6e0b1642045a4aff2b2aa231c
Duncan Bain <duncan@bain.io>
2020-05-12 20:56:10 +0000

    add repo and demo buttons to card

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import tw from "tailwind.macro";
 import { useStaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Card = styled.article`
   ${tw`w-full md:w-1/3 flex-auto rounded shadow-lg my-4 flex flex-col md:mx-4`}
@@ -13,6 +14,8 @@ const ProjectCard = ({
   projectImgAlt,
   projectTitle,
   projectCaption,
+  projectRepo,
+  projectDemo,
 }) => {
   const data = useStaticQuery(
     graphql`
@@ -50,6 +53,20 @@ const ProjectCard = ({
       <div className="px-6 py-4">
         <div className="font-bold text-2xl mb-2">{projectTitle}</div>
         <p className="text-gray-800 text-lg">{projectCaption}</p>
+      </div>
+      <div className="px-6 py-4 flex flex-row space-x-4 justify-center">
+        <a
+          href={projectRepo}
+          className="px-2 py-2 bg-blue-600 shadow-lg border rounded-lg text-white font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-500 active:bg-blue-500"
+        >
+          <FontAwesomeIcon icon={["fab", "github"]} /> Github Repo
+        </a>
+        <a
+          href={projectDemo}
+          className="px-4 py-2 bg-blue-600 shadow-lg border rounded-lg text-white font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-500 active:bg-blue-500"
+        >
+          Project Demo
+        </a>
       </div>
     </Card>
   );
