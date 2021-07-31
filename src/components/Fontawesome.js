1f923c5ffb3d39c67d9816dcfc0ce5770d9b666b
Duncan Bain <duncan@bain.io>
2021-07-31 18:19:28 +0100

    gatsby to nextjs from duncanbaindev-nextjs repo

--- src/components/Fontawesome.js
+++
@@ -1,9 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-
-import {
-  faGithub,
-  faTwitter,
-  faLinkedin,
-} from "@fortawesome/free-brands-svg-icons";
-
-library.add(faGithub, faTwitter, faLinkedin);
